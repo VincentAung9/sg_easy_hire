abb6f2c426500528ff104697c37b0f738c29d68a
VincentAung9 <aungbomyint.bm@gmail.com>
2025-12-18 18:47:46 +0700

    helper-home-50%

---
+++ lib/l10n/gen/app_localizations_en.dart
@@ -0,0 +1,64 @@
+// dart format off
+// coverage:ignore-file
+
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Easy Hire';
+
+  @override
+  String get appSubtitle => 'Find jobs in Singapore easily';
+
+  @override
+  String get welcomeBack => 'Welcome Back';
+
+  @override
+  String get signInSubtitle => 'Sign in to continue to your account';
+
+  @override
+  String get phoneNumber => 'Phone Number';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get enterPassword => 'Enter password';
+
+  @override
+  String get noAccount => 'Don\'t have an account?';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get signUpSubtitle => 'Create your account to get started';
+
+  @override
+  String get createAccountTitle => 'Create Account';
+
+  @override
+  String get createAccountSubtitle => 'Sign up with your phone number';
+
+  @override
+  String get fullNameLabel => 'Full Name';
+
+  @override
+  String get fullNameHint => 'Enter your full name';
+
+  @override
+  String get phoneNumberLabel => 'Phone Number';
+
+  @override
+  String get passwordLabel => 'Password';
+
+  @override
+  String get passwordHint => 'Enter password';
+}
